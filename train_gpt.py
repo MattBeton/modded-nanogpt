Small fixes trying to get optimizer state averaging working.

train_gpt.py
@@ -582,8 +582,8 @@ class Hyperparameters:
     interval_between_checkpoints = 50 # how often to save checkpoints for averaging
     update_interval = 50 # how often to update and evaluate the averaged model
     model_update_interval = 1000 # how often to update the model with the averaged model
-    model_average_timestep = 750
-    # model_average_timestep = 100
+    # model_average_timestep = 750
+    model_average_timestep = 100
 shared.args = Hyperparameters()
 args = shared.args
 
@@ -762,7 +762,7 @@ for step in range(train_steps + 1):
             checkpoint_dict = {
                 'step': step, 
                 'model_state_dict': cpu_state, 
-                # 'optimizer_state': [copy.deepcopy(x.state_dict()) for x in optimizers],
+                'optimizer_state': [copy.deepcopy(x.state_dict()) for x in optimizers],
             }
             checkpoint_list.append(checkpoint_dict)
             # Keep only the most recent num_checkpoints
@@ -795,7 +795,7 @@ for step in range(train_steps + 1):
 
         # we now have a dictionary of trajectory models to evaluate
     # --------------- VALIDATION SECTION  -----------------
-    if last_step or (args.val_loss_every > 0 and step % args.val_loss_every == 0):
+    if last_step or (args.val_loss_every > 0 and step % args.val_loss_every == 0) or ((step - args.model_average_timestep) < 50 and step > args.model_average_timestep):
         # stop the clock
         torch.cuda.synchronize()
         training_time_ms += 1000 * (time.perf_counter() - t0)
@@ -875,11 +875,11 @@ for step in range(train_steps + 1):
 
     if step == args.model_average_timestep:
         from utils import averaged_state_dict, load_state_dict_inplace
+        print0('averaging model and loading in!', console=True)
         avg_state = averaged_state_dict(checkpoint_list[-3:])
         load_state_dict_inplace(model, avg_state)
 
-        # model = average_models(model, checkpoint_list[-3:]) # memory unsafe atm it seems
-        # optimizers = average_optimizer_states(optimizers, checkpoint_list[-3:])
+        optimizers = average_optimizer_states(optimizers, checkpoint_list[-3:])
 
     if last_step:
         if master_process and args.save_checkpoint:

utils.py
@@ -56,7 +56,7 @@ def average_optimizer_states(optimizers, checkpoints: list, only_optimizers: lis
         # Zero out the state
         for key in state_dict['state']:
             for state_key, state_value in state_dict['state'][key].items():
-                if torch.is_tensor(state_value):
+                if torch.is_tensor(state_value) and torch.is_floating_point(state_value):
                     state_value.zero_()
         
         # Sum all checkpoint optimizer states
@@ -65,7 +65,7 @@ def average_optimizer_states(optimizers, checkpoints: list, only_optimizers: lis
             
             for key in checkpoint_opt_state['state']:
                 for state_key, state_value in checkpoint_opt_state['state'][key].items():
-                    if torch.is_tensor(state_value):
+                    if torch.is_tensor(state_value) and torch.is_floating_point(state_value):
                         if key not in state_dict['state']:
                             state_dict['state'][key] = {}
                         if state_key not in state_dict['state'][key]:
@@ -76,7 +76,7 @@ def average_optimizer_states(optimizers, checkpoints: list, only_optimizers: lis
         num_checkpoints = len(checkpoints)
         for key in state_dict['state']:
             for state_key, state_value in state_dict['state'][key].items():
-                if torch.is_tensor(state_value):
+                if torch.is_tensor(state_value) and torch.is_floating_point(state_value):
                     state_dict['state'][key][state_key] /= num_checkpoints
         
         optimizer.load_state_dict(state_dict)